00b673aa549d11d231fe204e8e18f772d78e3031
Chap Ambrose <chapambrose@gmail.com>
2021-04-01 13:02:16 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const Sequelize = require('sequelize');
+sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
+);
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
